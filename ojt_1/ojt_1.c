704b33616ae587578bb5dcf49cbaf3adfef4b0f0
손호철 <hc.son@stsunlab.com>
2024-05-31 20:12:28 +0900

    [OJT 1] 소스 최적화 참고용 브랜치 커밋
    1. while 문 띄어쓰기 적용
    
    2. 예외처리 위치 조정 및 에러 문구 수정
       - 리턴값 -1, -2 구분 처리
       - 예외처리 위치 main으로 이동
       - Segmentation fault 발생을 막아주는 예외처리 상황인데, "Error, Segmentation fault!" 발생했다는 로그가 출력됨
       - buffer overflow 발생을 막아주는 예외처리 상황인데, "Error, buffer overflowed!" 발생했다는 로그가 출력됨
    
    3. char copy[100] = {0}; 띄어쓰기 적용
    
    4. 에러 문구 재작성
    
    5. 주석 줄맞춤

--- ojt_1/ojt_1.c
+++ ojt_1/ojt_1.c
@@ -4,7 +4,7 @@
 int my_strlen(char str[])
 {
     int len = 0;
-    while(str[len] != '\0') // str[len]이 \0이 아니면 len 증가하면서 반복
+    while (str[len] != '\0')                 // str[len]이 \0이 아니면 len 증가하면서 반복
     {
         len++;
     }
@@ -17,16 +17,9 @@ int my_strcpy(char dest[], char src[])
     // len는 my_strlen함수를 통해 입력한 문자열의 길이가 담겨있음
     int len = my_strlen(src);
 
-    // 프로그램 죽지 않도록 예외처리, copy 배열의 최대 크기는 100
-    if (len >= 100)
+    for (int i = 0; i < len; i++)            // 문자열의 길이만큼 i가 증가하면서 반복
     {
-        printf("Error, buffer overflowed!\nPlease, input parameter under.\n");
-        return -1;
-    }
-
-    for (int i = 0; i < len; i++)                  // 문자열의 길이만큼 i가 증가하면서 반복
-    {
-        dest[i] = src[i];                          // 원본이 사본에 복사
+        dest[i] = src[i];                    // 원본이 사본에 복사
     }
 }
 
@@ -37,7 +30,7 @@ void my_strrev(char str[])
     int len = my_strlen(str);
 
     // 문자열 뒤집기
-    for (int i = 0; i < len / 2; i++)       // i<len/2는 길이가 6개면 3번 바꾸고, 길이가 5개면 2번 바꾸기 때문
+    for (int i = 0; i < (len / 2); i++)        // i<len/2는 길이가 6개면 3번 바꾸고, 길이가 5개면 2번 바꾸기 때문
     {
         char temp        = str[i];
         str[i]           = str[len - 1 - i]; // 뒤 단어가 앞으로 온다
@@ -48,23 +41,25 @@ void my_strrev(char str[])
 int main(int argc, char *argv[])
 {
     // 지역변수인 copy 배열 0으로 초기화
-    char copy[100]={0};
+    char copy[100] = {0};
 
     // argv[1]가 NULL인 경우 예외 처리
     if (argv[1] == NULL)
     {
-        printf("Error, Segmentation fault!\nPlease input parameter.\n");
+        printf("Error, input param is NULL!\nPlease, input parameter. (range: 1 ~ 99)\n");
         return -1;
+    }
+
+    // 프로그램 죽지 않도록 예외처리, copy 배열의 최대 크기는 100
+    int len = my_strlen(argv[1]);
+    if (len >= 100)
+    {
+        printf("Error, input param is overflowed! len = %d\nPlease, input parameter. (range: 1 ~ 99)\n", len);
+        return -2;
     }
 
     // 문자열 복사
     int result = my_strcpy(copy, argv[1]);
-
-    // buffer overflow 발생 시 프로그램 종료
-    if(result == -1)
-    {
-        return -1;
-    }
     printf("my_strcpy: %s\n", copy);
 
     // 문자열 뒤집기
