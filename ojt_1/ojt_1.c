1d8be6412d50c25765eb779081cc3cca4b573d7b
윤희연 <hy.youn@stsunlab.com>
2024-05-31 13:02:59 +0900

    [OJT 1] [#5] ojt_1 source optimization

--- ojt_1/ojt_1.c
+++ ojt_1/ojt_1.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
 
-//중복 코드 함수 (문자열 길이 계산)
-int my_strlen(char str[]){
+// 중복 코드 함수 (문자열 길이 계산)
+int my_strlen(char str[])
+{
     int len = 0;
-    while(str[len] != '\0') //str[len]이 \0이 아니면 len 증가하면서 반복
-    {    
-	len++;
+    while(str[len] != '\0')             // str[len]이 \0이 아니면 len 증가하면서 반복
+    {  
+        len++;
     }
     return len;
 }
 
 // 문자열 복사 함수
-void my_strcpy(char dest[], char src[]){ //src는 원본, dest는 사본
-    int len = my_strlen(src); //len는 my_strlen함수를 통해 입력한 문자열의 길이가 담겨있음
+void my_strcpy(char dest[], char src[]) // src는 원본, dest는 사본
+{
+    
+    int len = my_strlen(src);           // len는 my_strlen함수를 통해 입력한 문자열의 길이가 담겨있음
 
-    for (int i = 0; i < len; i++) { //문자열의 길이만큼 i가 증가하면서 반복
-        dest[i] = src[i]; //원본이 사본에 복사
+    for (int i = 0; i < len; i++)       // 문자열의 길이만큼 i가 증가하면서 반복
+    {
+        dest[i] = src[i];               // 원본이 사본에 복사
     }
 }
 
 // 문자열 뒤집기 함수
-void my_strrev(char str[]) {
-    int len = my_strlen(str); //len는 my_strlen함수를 통해 입력한 문자열의 길이가 담겨있음
+void my_strrev(char str[]) 
+{
+    int len = my_strlen(str);               // len는 my_strlen함수를 통해 입력한 문자열의 길이가 담겨있음
 
-    // 문자열 뒤집기
-    for (int i = 0; i < len / 2; i++) { //i<len/2는 길이가 6개면 3번 바꾸고, 길이가 5개면 2번 바꾸기 때문
-        char temp = str[i]; 
-        str[i] = str[len - 1 - i]; //뒤 단어가 앞으로 온다
-        str[len - 1 - i] = temp; //앞 단어가 뒤로 간다
+                                            // 문자열 뒤집기
+    for (int i = 0; i < len / 2; i++)       // i<len/2는 길이가 6개면 3번 바꾸고, 길이가 5개면 2번 바꾸기 때문  
+    { 
+	char temp        = str[i]; 
+        str[i]           = str[len - 1 - i]; // 뒤 단어가 앞으로 온다
+        str[len - 1 - i] = temp;             // 앞 단어가 뒤로 간다
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 
     char copy[100]; 
 
-    //문자열 복사
+    // 문자열 복사
     my_strcpy(copy, argv[1]);
     printf("my_strcpy: %s\n", copy);
 
-    //문자열 뒤집기
+    // 문자열 뒤집기
     my_strrev(argv[1]);
     printf("my_strrev: %s\n", argv[1]);
 
