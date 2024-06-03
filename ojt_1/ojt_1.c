5ed99604b374c41a2a9e0bffbab1518f267e4442
손호철 <hc.son@stsunlab.com>
2024-06-03 00:34:55 +0000

    [OJT 1] modify comment & white space

--- ojt_1/ojt_1.c
+++ ojt_1/ojt_1.c
@@ -26,11 +26,10 @@ int my_strcpy(char dest[], char src[])
 // 문자열 뒤집기 함수
 void my_strrev(char str[]) 
 {
-    // len는 my_strlen함수를 통해 입력한 문자열의 길이가 담겨있음
-    int len = my_strlen(str);
+    int len = my_strlen(str);                // len는 my_strlen함수를 통해 입력한 문자열의 길이가 담겨있음
 
     // 문자열 뒤집기
-    for (int i = 0; i < (len / 2); i++)        // i<len/2는 길이가 6개면 3번 바꾸고, 길이가 5개면 2번 바꾸기 때문
+    for (int i = 0; i < (len / 2); i++)      // i<len/2는 길이가 6개면 3번 바꾸고, 길이가 5개면 2번 바꾸기 때문
     {
         char temp        = str[i];
         str[i]           = str[len - 1 - i]; // 뒤 단어가 앞으로 온다
@@ -40,17 +39,16 @@ void my_strrev(char str[])
 
 int main(int argc, char *argv[])
 {
-    // 지역변수인 copy 배열 0으로 초기화
-    char copy[100] = {0};
+    char copy[100] = {0};                    // 지역변수인 copy 배열 0으로 초기화
 
-    // argv[1]가 NULL인 경우 예외 처리
+    // Exception : argv[1]가 NULL인 경우 예외 처리
     if (argv[1] == NULL)
     {
         printf("Error, input param is NULL!\nPlease, input parameter. (range: 1 ~ 99)\n");
         return -1;
     }
 
-    // 프로그램 죽지 않도록 예외처리, copy 배열의 최대 크기는 100
+    // Exception : 프로그램 죽지 않도록 예외처리, copy 배열의 최대 크기는 100
     int len = my_strlen(argv[1]);
     if (len >= 100)
     {
