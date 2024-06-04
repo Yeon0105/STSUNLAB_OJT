6fb3c024ff6f2ba779c91004f9afca0a5d263da8
윤희연 <hy.youn@stsunlab.com>
2024-06-04 17:59:13 +0900

    [OJT 2][#8] ojt_2 source optimization

--- ojt_2/ojt_2.c
+++ ojt_2/ojt_2.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 
-#define LCD_WIDTH 4                          // 가로 4
-#define LCD_HEIGHT 8                         // 세로 8
-#define DRAW_MODE 1
+#define LCD_WIDTH    4                       // 가로 4
+#define LCD_HEIGHT   8                       // 세로 8
+#define DRAW_MODE    1
+
 
 int display_buffer[LCD_HEIGHT * LCD_WIDTH];  // display buffer
 
 void fill_display_buffer()
 {
+#if DRAW_MODE
     // DRAW_MODE 상태라면 조건문 실행
-    #if DRAW_MODE
-        // 그림 모드 : 'ㄱ' 모양 데이터 삽입
-        for (int i = 0; i < LCD_HEIGHT * LCD_WIDTH; i++)
+    // 그림 모드 : 'ㄱ' 모양 데이터 삽입
+    for (int i = 0; i < LCD_HEIGHT * LCD_WIDTH; i++)
+    {
+        // 첫 행과 마지막 열 위치인지 확인하는 조건문
+        if (i < LCD_WIDTH || i % LCD_WIDTH == LCD_WIDTH - 1)
         {
-            // 첫 행과 마지막 열 위치인지 확인하는 조건문
-            if (i < LCD_WIDTH || i % LCD_WIDTH == LCD_WIDTH - 1)
-            {
-                display_buffer[i] = 11;
-            }
-            else
-            {
-                display_buffer[i] = 0;
-            }
+            display_buffer[i] = 11;
         }
+        else
+        {
+            display_buffer[i] = 0;
+        }
+    }
+#else
     // DRAW_MODE 상태가 아니면 조건문 실행
-    #else
-        // 숫자 모드 : 1부터 32까지 채우는 함수
-        int number = 1;
-            for (int i = 0; i < LCD_HEIGHT * LCD_WIDTH; i++)
-            {
-                display_buffer[i] = number++;
-            }
-    #endif
+    // 숫자 모드 : 1부터 32까지 채우는 함수
+    int number = 1;
+    for (int i = 0; i < LCD_HEIGHT * LCD_WIDTH; i++)
+    {
+        display_buffer[i] = number++;
+    }
+#endif
 }
 
 // 원본
@@ -116,14 +117,14 @@ void print_display_buffer(int rotation)
 }
 
 // 메인함수
-int main()
+int main(void)
 {
     fill_display_buffer();
 
-    printf("> normal\n");     // 원본 출력
+    printf("> normal\n");      // 원본 출력
     print_display_buffer(0);
 
-    printf("> rotate 90\n");  // 90도 회전 출력
+    printf("> rotate 90\n");   // 90도 회전 출력
     print_display_buffer(90);
 
     printf("> rotate 180\n");  // 180도 회전 출력
@@ -131,5 +132,6 @@ int main()
 
     printf("> rotate 270\n");  // 270도 회전 출력
     print_display_buffer(270);
+
     return 0;
 }
