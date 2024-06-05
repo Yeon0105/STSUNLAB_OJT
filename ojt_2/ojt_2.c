0f8074099730c98873b99d48530bd1e093a27531
윤희연 <hy.youn@stsunlab.com>
2024-06-05 09:42:48 +0900

    [OJT 2][#11] ojt_2 source optimization edit

--- ojt_2/ojt_2.c
+++ ojt_2/ojt_2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
+#define DRAW_MODE                            // DRAW_MODE 매크로 정의
+
 #define LCD_WIDTH    4                       // 가로 4
 #define LCD_HEIGHT   8                       // 세로 8
-#define DRAW_MODE
 
 int display_buffer[LCD_HEIGHT * LCD_WIDTH];  // display buffer
 
-void fill_display_buffer()
+void fill_display_buffer(void)
 {
 #ifdef DRAW_MODE
-{
     // DRAW_MODE 매크로가 정의되어 있으면 조건문 실행
     // 그림 모드 : 'ㄱ' 모양 데이터 삽입
-    for (int i = 0; i < LCD_HEIGHT * LCD_WIDTH; i++)
+    for (int i = 0; i < (LCD_HEIGHT * LCD_WIDTH); i++)
     {
         // 첫 행과 마지막 열 위치인지 확인하는 조건문
-        if (i < LCD_WIDTH || i % LCD_WIDTH == LCD_WIDTH - 1)
+        if ((i < LCD_WIDTH) || ((i % LCD_WIDTH) == (LCD_WIDTH - 1)))
         {
             display_buffer[i] = 11;
         }
@@ -24,12 +24,11 @@ void fill_display_buffer()
             display_buffer[i] = 0;
         }
     }
-}
 #else
     // DRAW_MODE 매크로가 정의되어 않으면 조건문 실행
     // 숫자 모드 : 1부터 32까지 채우는 함수
     int number = 1;
-    for (int i = 0; i < LCD_HEIGHT * LCD_WIDTH; i++)
+    for (int i = 0; i < (LCD_HEIGHT * LCD_WIDTH); i++)
     {
         display_buffer[i] = number++;
     }
@@ -37,14 +36,13 @@ void fill_display_buffer()
 }
 
 // 원본
-void print_normal()
+void print_normal(void)
 {
     for (int i = 0; i < LCD_HEIGHT; i++)
     {
         for (int j = 0; j < LCD_WIDTH; j++)
         {
-            int index = (i * LCD_WIDTH) + j;       // 01 = [0]
-            printf("%02d", display_buffer[index]); // 2자리 수로 정렬하여 출력, 행마다 줄바꿈 수행
+            printf("%02d", display_buffer[(i * LCD_WIDTH) + j]);  // 2자리 수로 정렬하여 출력, 행마다 줄바꿈 수행, 01 = [0]
         }
         printf("\n");
     }
@@ -52,14 +50,13 @@ void print_normal()
 }
 
 // 90도 회전
-void print_rotate_90()
+void print_rotate_90(void)
 {
     for (int i = 0; i < LCD_WIDTH; i++)
     {
-        for (int j = LCD_HEIGHT - 1; j >= 0; j--)  // 역순 탐색
+        for (int j = LCD_HEIGHT - 1; j >= 0; j--)                 // 역순 탐색
         {
-            int index = (j * LCD_WIDTH) + i;       // 29 = [28]
-            printf("%02d", display_buffer[index]);
+            printf("%02d", display_buffer[(j * LCD_WIDTH) + i]);  // 29 = [28]
         }
         printf("\n");
     }
@@ -67,14 +64,13 @@ void print_rotate_90()
 }
 
 // 180도 회전
-void print_rotate_180()
+void print_rotate_180(void)
 {
     for (int i = LCD_HEIGHT - 1; i >= 0; i--)
     {
         for (int j = LCD_WIDTH - 1; j >= 0; j--)
         {
-            int index = (i * LCD_WIDTH) + j;     // 32 = [31]
-            printf("%02d", display_buffer[index]);
+            printf("%02d", display_buffer[(i * LCD_WIDTH) + j]);  // 32 = [31]
         }
         printf("\n");
     }
@@ -82,14 +78,13 @@ void print_rotate_180()
 }
 
 // 270도 회전
-void print_rotate_270()
+void print_rotate_270(void)
 {
     for (int i = LCD_WIDTH - 1; i >= 0; i--)
     {
         for (int j = 0; j < LCD_HEIGHT; j++)
         {
-            int index = (j * LCD_WIDTH) + i;     // 04 = [3]
-            printf("%02d", display_buffer[index]);
+            printf("%02d", display_buffer[(j * LCD_WIDTH) + i]);  // 04 = [3]
         }
         printf("\n");
     }
