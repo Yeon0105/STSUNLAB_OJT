746c7cdd7394d8a9d4a3257c6ba1577d4430d136
윤희연 <hy.youn@stsunlab.com>
2024-06-10 11:45:35 +0900

    [OJT 3] [#14] 프로그램 결과 출력 최적화 1. 입력 범위를 1~10 으로 설정 2. 범위에 벗어나는 숫자 입력 시, 오류 메시지 출력

--- ojt_3/ojt_3.c
+++ ojt_3/ojt_3.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>                         // 동적 메모리 할당 함수 사용을 위한 헤더 파일
 
 // 달팽이 배열을 채우는 함수
-void fill_spiral_array(int **arr, int row, int col)
+void fillSpiralArray(int **arr, int row, int col)
 {
     int num    = 0;                         // 배열에 채울 숫자
     int x      = 0;                         // 현재 위치를 나타냄, [0][y]
     int y      = -1;                        // 현재 위치를 나타냄, [x][-1]
-    int target = 1;                         // 방향을 나타내는 변수, 1이면 오른쪽 또는 아래, -1이면 왼쪽 또는 위로 이동
+    int t      = 1;                         // 방향을 나타내는 변수, 1이면 오른쪽 또는 아래, -1이면 왼쪽 또는 위로 이동
     int maxRow = row;                       // 채워야 할 행의 최대 개수
     int maxCol = col;                       // 채워야 할 열의 최대 갯수 
 
-    while ((maxCol > 0) && (maxRow > 0))    // 채워야 할 행,열이 남아 있을 때 까지 반복 
+    while (maxCol > 0 && maxRow > 0)        // 채워야 할 행,열이 남아 있을 때 까지 반복 
     {
         for (int i = 0; i < maxCol; i++)    // 현재 행에서 열을 따라 이동하면서 값 채우기, t가 1이면 오른쪽, -1이면 왼쪽 이동
         {
-            y += target;                    // y값을 증가 또는 감소 시켜 이동
+            y += t;                         // y값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
-        maxRow--;                           // 한 줄 채웠으면 남은 행 수 줄이기
+        maxRow--;                           // 한 줄 채웠으면 남은 행 수 줄이기 
 
         for (int i = 0; i < maxRow; i++)    // 현재 열에서 행을 따라 이동하면서 값 채우기, t가 1이면 아래, -1이면 위로 이동
         {
-            x += target;                    // x값을 증가 또는 감소 시켜 이동
+            x += t;                         // x값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
         maxCol--;                           // 한 줄을 채웠으면 남은 열 수 줄이기
 
-        target = -target;                   // 방향 반대 바꾸기
+        t = -t;                             // 방향 반대 바꾸기
     }
 }
 
 // 구분선 출력하는 함수
-void print_line(int col)
+void printLine(int col)
 {
-    for (int i = 0; i < col; i++)
+    for (int i = 0; i < col; i++) 
     {
         printf("----");
     }
-    printf("\n");
+    printf("\n");    
 }
 
 // 배열 출력하는 함수
-void print_array(int **arr, int row, int col)
+void printArray(int **arr, int row, int col)
 {
     // ---- 출력
-    print_line(col);
-
+    printLine(col);
     // 2차원 배열 출력하는 반복문
     for (int i = 0; i < row; i++)
     {
@@ -56,15 +55,14 @@ void print_array(int **arr, int row, int col)
         }
         printf("\n");
     }
-
     // ---- 출력
-    print_line(col);
+    printLine(col);
 }
 
-// 입력 버퍼를 비우는 함수
+// 입력 버퍼 비우는 함수
 void clear_input_buffer()
 {
-    while (getchar() != '\n');
+    while (getchar () != '\n');
 }
 
 // 유효한 정수를 입력받는 함수
@@ -75,17 +73,29 @@ int get_valid_integer(char *prompt)
 
     while (1)
     {
-        printf("%s", prompt);                                               // 전달된 문자열 row? 또는 col?
-        if ((scanf("%d%c", &value, &c) == 2) && (c == '\n') && (value > 0)) // 2개의 값을 읽어 2를 반환, 문자가 \n, 정수가 양수여야 하는 조건문
+        printf("%s", prompt);                                                           // 전달된 문자열 row? 또는 col?
+        if ((scanf("%d%c", &value, &c) == 2) && (c == '\n') && (value > 0))             // 2개의 값을 읽어 2를 반환, 문자가 \n, 정수가 양수여야 하는 조건문
         {
-            return value;
+            if((value > 0) &&(value <= 10))                                             // 유효범위 1~10 설정, 최대 10행 10열까지 가능
+            {
+                return value;
+            }
+            else
+            {
+                printf("Error, input integer too big!\nPlease, input integer 1~10.\n"); // 10 이상 입력 시 에러 출력 
+            }
         }
-        printf("Error, Invalid input!\nPlease, input positive integer.\n"); // 위 조건을 만족하지 않으면 에러 출력
+        else
+        {
+            printf("Error, invalid input!\nPlease, input positive integer.\n");         // 위 조건을 만족하지 않으면 에러 출력
+        }
         clear_input_buffer();
     }
 }
 
-int main(void) {
+int main(void)
+{
+    // 사용자로부터 행과 열 입력 받기
     int row = get_valid_integer("row? ");
     int col = get_valid_integer("col? ");
 
@@ -97,10 +107,10 @@ int main(void) {
     }
 
     // 배열을 달팽이 모양으로 채우기
-    fill_spiral_array(arr, row, col);
+    fillSpiralArray(arr, row, col);
 
     // 배열 출력
-    print_array(arr, row, col);
+    printArray(arr, row, col);
 
     // 동적 메모리 해제
     for (int i = 0; i < row; i++)
