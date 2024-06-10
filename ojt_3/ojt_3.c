f87dbf6f9098b8ab1e65dbe0fb4e12a117980e8f
윤희연 <hy.youn@stsunlab.com>
2024-06-10 13:04:34 +0900

    [OJT 3] [#15] 소스 가독성 개선 1. 괄호 묶기 2. 함수명 _ 이용하여 소문자 적용 3. 의미있는 변수명 사용 4. 주석으로 설명하는 로직 별 엔터 줄바꿈 구분 5. 지역 변수 초기화 후 사용

--- ojt_3/ojt_3.c
+++ ojt_3/ojt_3.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>                         // 동적 메모리 할당 함수 사용을 위한 헤더 파일
 
 // 달팽이 배열을 채우는 함수
-void fillSpiralArray(int **arr, int row, int col)
+void fill_spiral_array(int **arr, int row, int col)
 {
     int num    = 0;                         // 배열에 채울 숫자
     int x      = 0;                         // 현재 위치를 나타냄, [0][y]
     int y      = -1;                        // 현재 위치를 나타냄, [x][-1]
-    int t      = 1;                         // 방향을 나타내는 변수, 1이면 오른쪽 또는 아래, -1이면 왼쪽 또는 위로 이동
+    int target = 1;                         // 방향을 나타내는 변수, 1이면 오른쪽 또는 아래, -1이면 왼쪽 또는 위로 이동
     int maxRow = row;                       // 채워야 할 행의 최대 개수
     int maxCol = col;                       // 채워야 할 열의 최대 갯수 
 
-    while (maxCol > 0 && maxRow > 0)        // 채워야 할 행,열이 남아 있을 때 까지 반복 
+    while ((maxCol > 0) && (maxRow > 0))    // 채워야 할 행,열이 남아 있을 때 까지 반복 
     {
         for (int i = 0; i < maxCol; i++)    // 현재 행에서 열을 따라 이동하면서 값 채우기, t가 1이면 오른쪽, -1이면 왼쪽 이동
         {
-            y += t;                         // y값을 증가 또는 감소 시켜 이동
+            y += target;                    // y값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
         maxRow--;                           // 한 줄 채웠으면 남은 행 수 줄이기 
 
         for (int i = 0; i < maxRow; i++)    // 현재 열에서 행을 따라 이동하면서 값 채우기, t가 1이면 아래, -1이면 위로 이동
         {
-            x += t;                         // x값을 증가 또는 감소 시켜 이동
+            x += target;                    // x값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
         maxCol--;                           // 한 줄을 채웠으면 남은 열 수 줄이기
 
-        t = -t;                             // 방향 반대 바꾸기
+        target = -target;                   // 방향 반대 바꾸기
     }
 }
 
 // 구분선 출력하는 함수
-void printLine(int col)
+void print_line(int col)
 {
     for (int i = 0; i < col; i++) 
     {
@@ -42,10 +42,11 @@ void printLine(int col)
 }
 
 // 배열 출력하는 함수
-void printArray(int **arr, int row, int col)
+void print_array(int **arr, int row, int col)
 {
     // ---- 출력
-    printLine(col);
+    print_line(col);
+
     // 2차원 배열 출력하는 반복문
     for (int i = 0; i < row; i++)
     {
@@ -55,12 +56,13 @@ void printArray(int **arr, int row, int col)
         }
         printf("\n");
     }
+
     // ---- 출력
-    printLine(col);
+    print_line(col);
 }
 
 // 입력 버퍼 비우는 함수
-void clear_input_buffer()
+void clear_input_buffer(void)
 {
     while (getchar () != '\n');
 }
@@ -107,10 +109,10 @@ int main(void)
     }
 
     // 배열을 달팽이 모양으로 채우기
-    fillSpiralArray(arr, row, col);
+    fill_spiral_array(arr, row, col);
 
     // 배열 출력
-    printArray(arr, row, col);
+    print_array(arr, row, col);
 
     // 동적 메모리 해제
     for (int i = 0; i < row; i++)
