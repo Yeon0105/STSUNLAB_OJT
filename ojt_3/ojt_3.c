02a0b8f6831b4d930c27450918e48256980fe3bd
윤희연 <hy.youn@stsunlab.com>
2024-06-10 14:54:07 +0900

    [OJT 3] [#17] 소스 최적화 1. 단어 사이에 언더바를 넣어 표기하는 스네이크 표기법 사용 2. 함수 호출 시 괄호 붙여 사용 3. 무한 루프 가급적 사용 금지

--- ojt_3/ojt_3.c
+++ ojt_3/ojt_3.c
@@ -8,24 +8,24 @@ void fill_spiral_array(int **arr, int row, int col)
     int x      = 0;                         // 현재 위치를 나타냄, [0][y]
     int y      = -1;                        // 현재 위치를 나타냄, [x][-1]
     int target = 1;                         // 방향을 나타내는 변수, 1이면 오른쪽 또는 아래, -1이면 왼쪽 또는 위로 이동
-    int maxRow = row;                       // 채워야 할 행의 최대 개수
-    int maxCol = col;                       // 채워야 할 열의 최대 갯수 
+    int max_row = row;                       // 채워야 할 행의 최대 개수
+    int max_col = col;                       // 채워야 할 열의 최대 갯수 
 
-    while ((maxCol > 0) && (maxRow > 0))    // 채워야 할 행,열이 남아 있을 때 까지 반복 
+    while ((max_col > 0) && (max_row > 0))    // 채워야 할 행,열이 남아 있을 때 까지 반복 
     {
-        for (int i = 0; i < maxCol; i++)    // 현재 행에서 열을 따라 이동하면서 값 채우기, t가 1이면 오른쪽, -1이면 왼쪽 이동
+        for (int i = 0; i < max_col; i++)    // 현재 행에서 열을 따라 이동하면서 값 채우기, t가 1이면 오른쪽, -1이면 왼쪽 이동
         {
             y += target;                    // y값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
-        maxRow--;                           // 한 줄 채웠으면 남은 행 수 줄이기 
+        max_row--;                           // 한 줄 채웠으면 남은 행 수 줄이기 
 
-        for (int i = 0; i < maxRow; i++)    // 현재 열에서 행을 따라 이동하면서 값 채우기, t가 1이면 아래, -1이면 위로 이동
+        for (int i = 0; i < max_row; i++)    // 현재 열에서 행을 따라 이동하면서 값 채우기, t가 1이면 아래, -1이면 위로 이동
         {
             x += target;                    // x값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
-        maxCol--;                           // 한 줄을 채웠으면 남은 열 수 줄이기
+        max_col--;                           // 한 줄을 채웠으면 남은 열 수 줄이기
 
         target = -target;                   // 방향 반대 바꾸기
     }
@@ -61,37 +61,29 @@ void print_array(int **arr, int row, int col)
     print_line(col);
 }
 
-// 입력 버퍼 비우는 함수
-void clear_input_buffer(void)
-{
-    while (getchar () != '\n');
-}
-
 // 유효한 정수를 입력받는 함수
 int get_valid_integer(char *prompt)
 {
     char c    = '\n';
     int value = 0;
 
-    while (1)
+    printf("%s", prompt);                                                           // 전달된 문자열 row? 또는 col?
+    if ((scanf("%d%c", &value, &c) == 2) && (c == '\n') && (value > 0))             // 2개의 값을 읽어 2를 반환, 문자가 \n, 정수가 양수여야 하는 조건문
     {
-        printf("%s", prompt);                                                           // 전달된 문자열 row? 또는 col?
-        if ((scanf("%d%c", &value, &c) == 2) && (c == '\n') && (value > 0))             // 2개의 값을 읽어 2를 반환, 문자가 \n, 정수가 양수여야 하는 조건문
+        if((value > 0) &&(value <= 10))                                             // 유효범위 1~10 설정, 최대 10행 10열까지 가능
         {
-            if((value > 0) &&(value <= 10))                                             // 유효범위 1~10 설정, 최대 10행 10열까지 가능
-            {
-                return value;
-            }
-            else
-            {
-                printf("Error, input integer too big!\nPlease, input integer 1~10.\n"); // 10 이상 입력 시 에러 출력 
-            }
+            return value;
         }
         else
         {
-            printf("Error, invalid input!\nPlease, input positive integer.\n");         // 위 조건을 만족하지 않으면 에러 출력
+            printf("Error, input integer too big!\nPlease, input integer 1~10.\n"); // 10 이상 입력 시 에러 출력
+            exit(1);
         }
-        clear_input_buffer();
+    }
+    else
+    {
+        printf("Error, invalid input!\nPlease, input positive integer.\n");         // 위 조건을 만족하지 않으면 에러 출력
+        exit(1);
     }
 }
 
