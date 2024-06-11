813474ea554606c1abf03019939d8fa7a04a2207
윤희연 <hy.youn@stsunlab.com>
2024-06-11 10:21:35 +0900

    [OJT 3] [#18] memory leakage 관련 고민 (소스 안정화)
    1. #define은 #include 다음에 배치
    2. TRUE/FALSE 매크로 정의
    3. fill_spiral_array() 에서 불필요한 max_row, max_col 지역변수 제거
    4. error code를 먼저 예외처리 하는 순서대로 -1, -2 넘버링
    5. check_index 사용하도록 수정
    6. 동적 메모리 할당/해제 관련 소스 안정화
    
    동적 메모리 할당은 주로 힙(heap) 메모리를 사용한다.
    즉, 프로그램 실행 시에 크기와 할당을 결정한다.
    
    동적 메모리의 초기화는 calloc 나 memset 을 사용한다.
    calloc 는 메모리를 할당하고 동시에 0으로 초기화한다.
    memset 는 이미 할당된 메모리를 특정 값으로 초기화한다.

--- ojt_3/ojt_3.c
+++ ojt_3/ojt_3.c
@@ -1,35 +1,37 @@
-#define MAX_SIZE 15                         // 입력 제한 매크로 정의
-
 #include <stdio.h>
-#include <stdlib.h>                         // 동적 메모리 할당 함수 사용을 위한 헤더 파일
+#include <stdlib.h>                            // 동적 메모리 할당 함수 사용을 위한 헤더 파일
+#include <string.h>                            // memset 함수를 사용하기 위한 헤더 파일
+
+#define MAX_SIZE 10                            // 입력 제한 매크로 정의
+
+#define TRUE 1
+#define FALSE 0
 
 // 달팽이 배열을 채우는 함수
-void fill_spiral_array(int **arr, int row, int col)
+void fill_spiral_array(int **arr, int max_row, int max_col)
 {
-    int num     = 0;                         // 배열에 채울 숫자
-    int x       = 0;                         // 현재 위치를 나타냄, [0][y]
-    int y       = -1;                        // 현재 위치를 나타냄, [x][-1]
-    int target  = 1;                         // 방향을 나타내는 변수, 1이면 오른쪽 또는 아래, -1이면 왼쪽 또는 위로 이동
-    int max_row = row;                       // 채워야 할 행의 최대 개수
-    int max_col = col;                       // 채워야 할 열의 최대 갯수 
+    int num    = 0;                             // 배열에 채울 숫자
+    int x      = 0;                             // 현재 위치를 나타냄, [0][y]
+    int y      = -1;                            // 현재 위치를 나타냄, [x][-1]
+    int target = 1;                             // 방향을 나타내는 변수, 1이면 오른쪽 또는 아래, -1이면 왼쪽 또는 위로 이동
 
-    while ((max_col > 0) && (max_row > 0))   // 채워야 할 행,열이 남아 있을 때 까지 반복 
+    while ((max_col > 0) && (max_row > 0))      // 채워야 할 행, 열이 남아 있을 때까지 반복
     {
-        for (int i = 0; i < max_col; i++)    // 현재 행에서 열을 따라 이동하면서 값 채우기, t가 1이면 오른쪽, -1이면 왼쪽 이동
+        for (int i = 0; i < max_col; i++)       // 현재 행에서 열을 따라 이동하면서 값 채우기, target이 1이면 오른쪽, -1이면 왼쪽 이동
         {
-            y += target;                     // y값을 증가 또는 감소 시켜 이동
+            y += target;                        // y값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
-        max_row--;                           // 한 줄 채웠으면 남은 행 수 줄이기 
+        max_row--;                              // 한 줄 채웠으면 남은 행 수 줄이기
 
-        for (int i = 0; i < max_row; i++)    // 현재 열에서 행을 따라 이동하면서 값 채우기, t가 1이면 아래, -1이면 위로 이동
+        for (int i = 0; i < max_row; i++)       // 현재 열에서 행을 따라 이동하면서 값 채우기, target이 1이면 아래, -1이면 위로 이동
         {
-            x += target;                     // x값을 증가 또는 감소 시켜 이동
+            x += target;                        // x값을 증가 또는 감소 시켜 이동
             arr[x][y] = num++;
         }
-        max_col--;                           // 한 줄을 채웠으면 남은 열 수 줄이기
+        max_col--;                              // 한 줄을 채웠으면 남은 열 수 줄이기
 
-        target = -target;                    // 방향 반대 바꾸기
+        target = -target;                       // 방향 반대 바꾸기
     }
 }
 
@@ -72,20 +74,20 @@ int get_valid_integer(char *prompt)
     printf("%s", prompt);                                                           // 전달된 문자열 row? 또는 col?
     if ((scanf("%d%c", &value, &c) == 2) && (c == '\n') && (value > 0))             // 2개의 값을 읽어 2를 반환, 문자가 \n, 정수가 양수여야 하는 조건문
     {
-        if ((value > 0) &&(value <= MAX_SIZE))                                      // 유효범위 1~MAX_SIZE 설정
+        if ((value > 0) && (value <= MAX_SIZE))                                     // 유효범위 1~MAX_SIZE 설정
         {
             return value;
         }
         else
         {
             printf("Error, input integer too big!\n");                              // MAX_SIZE 보다 큰 숫자 입력 시 에러 출력
-            return -3;
+            return -1;
         }
     }
     else
     {
-        printf("Error, invalid input!\n");                                          // 위 조건을 만족하지 않으면 에러 출력
-        return -4;
+        printf("Error, invalid input!\n");                                          // 음수나 알파벳 입력 시 에러 출력
+        return -2;
     }
 }
 
@@ -95,22 +97,14 @@ int main(void)
     int row = get_valid_integer("row? ");
 
     // 동적 메모리 체크하는 변수
-    char check_error = 0;
-    int check_index = 0;
+    char check_error = FALSE;
+    int  check_index = 0;
 
     // get_valid_integer 함수에서 row의 에러가 발생한 경우 
     if (row < 0)
     {
-        if (row == -3)
-        {
-            printf("Please, row input integer 1~%d.\n", MAX_SIZE);
-            return -3;
-        }
-        else if (row == -4)
-        {
-            printf("Please, row input positive integer.\n");
-            return -4;
-        }
+        printf("Please, row input integer 1~%d.\n", MAX_SIZE);
+        return -1;
     }
 
     // 사용자로부터 열 입력 받기
@@ -119,16 +113,8 @@ int main(void)
     // get_valid_integer 함수에서 col의 에러가 발생한 경우
     if (col < 0)
     {
-        if (col == -3)
-        {
-            printf("Please, col input integer 1~%d.\n", MAX_SIZE);
-            return -3;
-        }
-        else if (col == -4)
-        {
-            printf("Please, col input positive integer.\n");
-            return -4;
-        }
+        printf("Please, col input integer 1~%d.\n", MAX_SIZE);
+        return -2;
     }
 
     // 동적 메모리 할당
@@ -137,8 +123,8 @@ int main(void)
     // 동적 메모리 할당 실패 시 오류
     if (arr == NULL)
     {
-        printf("Memory allocation failed.\n");
-        return -1;
+        printf("Error, row memory allocation failed.\n");
+        return -3;
     }
 
     for (int i = 0; i < row; i++)
@@ -148,25 +134,37 @@ int main(void)
         // 동적 메모리 할당 실패 시 오류
         if (arr[i] == NULL)
         {
-            printf("Memory allocation failed.\n");
+            printf("Error, col memory allocation failed.\n");
 
-            check_error = 1;
+            check_error = TRUE;
             check_index = i;
 
             break;
         }
+
+        // 할당받은 메모리 초기화
+        memset(arr[i], 0, sizeof(int) * col);
     }
 
     // 동적 메모리 할당 실패 시 free
     if (check_error)
     {
-        for (int i=0; i < row; i++)
+        // 달팽이 배열 가로열 동적 메모리 해제
+        for (int i = 0; i < check_index; i++) // 0부터 check_index-1 까지 반복
         {
-            free(arr[i]);
+            if (arr[i] != NULL)
+            {
+                free(arr[i]);
+            }
         }
-        free(arr);
 
-        return -2;
+        // 달팽이 배열 세로열 동적 메모리 해제
+        if (arr != NULL)
+        {
+            free(arr);
+        }
+
+        return -4;
     }
 
     // 배열을 달팽이 모양으로 채우기
@@ -175,12 +173,20 @@ int main(void)
     // 배열 출력
     print_array(arr, row, col);
 
-    // 정상적으로 실행 된 동적 메모리에 대한 free
+    // 달팽이 배열 가로열 동적 메모리 해제
     for (int i = 0; i < row; i++)
     {
-        free(arr[i]);
+        if (arr[i] != NULL)
+        {
+            free(arr[i]);
+        }
     }
-    free(arr);
+
+    // 달팽이 배열 세로열 동적 메모리 해제
+    if (arr != NULL)
+    {
+        free(arr);
+    }
 
     return 0;
 }
