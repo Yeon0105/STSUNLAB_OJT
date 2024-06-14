1c77e22055b126c4950c93c22204151e703c1f49
윤희연 <hy.youn@stsunlab.com>
2024-06-14 10:35:31 +0900

    [OJT 4] [#22] enum type을 이용한 소스 최적화 1. enum type을 이용하여 각 예외처리 상황에 맞춰 return 처리

--- ojt_4/ojt_4.c
+++ ojt_4/ojt_4.c
@@ -3,6 +3,22 @@
 #include <ctype.h>                     // isdigit를 쓰기 위한 헤더 파일 선언
 #include <limits.h>                    // INT_MAX, INT_MAX를 쓰기 위한 헤더 파일 선언
 
+// enum 이용하여 예외상황 처리
+enum ERROR_EXCEPTION_HANDLING
+{
+    ERROR_NONE                     = 0,
+    OPERAND_CHECK_NUM_TRUE         = 1,
+    ERROR_LESS_THAN_4_OPERANDS     = -1,
+    ERROR_MORE_THAN_4_OPERANDS     = -2,
+    ERROR_FIRST_OPERAND_NOT_NUM    = -3,
+    ERROR_SECOND_OPERAND_NOT_NUM_  = -4,
+    ERROR_OPERAND_EXCEED_INT_RANGE = -5,
+    ERROR_INPUT_2_OPERATORS        = -6,
+    ERROR_NOT_DIVIDE_BY_0          = -7,
+    ERROR_NOT_4_BASIC_OPERATORS    = -8,
+    ERROR_FOPEN_FAIL               = -9
+};
+
 // 덧셈 함수
 int add(int operand_1, int operand_2)
 {
@@ -42,13 +58,13 @@ int check_number(char *str)
         // 숫자가 아니면 0 반환, isdigit 함수는 숫자로 변경 실패 시 0 (false) 반환
         if (!isdigit(*str))
         {
-            return 0;
+            return ERROR_NONE;
         }
         str++;
     }
 
     // 인자가 숫자이면 1 (true) 반환
-    return 1;
+    return OPERAND_CHECK_NUM_TRUE;
 }
 
 // 문자열 길이 strlen 계산 함수
@@ -73,6 +89,7 @@ int my_strlen(char str[])
 6. [예외처리] 나눗셈 연산 시 두번째 피연산자가 0이면 예외처리로 에러 메시지 출력.
 7. [코드구현] 사칙연산 계산 처리 및 결과 출력.
 8. [코드구현] 사칙연산 계산기를 실행한 결과가 cal_result.txt 파일에 저장. (계산 결과값이 마지막 줄에 누적되어 저장됨)
+9. [enum 이용 예외처리] enum type을 이용하여 각 예외처리 상황의 return 값 처리.
 */
 
 int main(int argc, char *argv[])
@@ -81,14 +98,14 @@ int main(int argc, char *argv[])
     if (argc < 4)
     {
         printf("Error, few operands.\n");
-        return -1;
+        return ERROR_LESS_THAN_4_OPERANDS;
     }
 
     // 인자 수 4보다 많으면 에러
     if (argc > 4)
     {
         printf("Error, many operands.\n");
-        return -2;
+        return ERROR_MORE_THAN_4_OPERANDS;
     }
     
     int first_operand  = atoi(argv[1]);  // 첫 번째 피연산자, 문자열을 정수로 변환
@@ -104,40 +121,40 @@ int main(int argc, char *argv[])
     if (!check_number(argv[1]))
     {
         printf("Error, first operand is not a number.\n");
-        return -3;
+        return ERROR_FIRST_OPERAND_NOT_NUM;
     }
 
     // 두 번째 피연산자가 숫자가 아닌 경우 에러
     if (!check_number(argv[3]))
     {
         printf("Error, second operand is not a number.\n");
-        return -4;
+        return ERROR_SECOND_OPERAND_NOT_NUM_;
     }
 
     // 피연산자가 int 범위를 벗어나면 에러
     if ((first_operand > INT_MAX) || (second_operand > INT_MAX))
     {
         printf("Error, operand out of range for int type.\n");
-        return -5;
+        return ERROR_OPERAND_EXCEED_INT_RANGE;
     }
     else if ((first_operand < INT_MIN) || (second_operand < INT_MIN))
     {
         printf("Error, operand out of range for int type.\n");
-        return -6;
+        return ERROR_OPERAND_EXCEED_INT_RANGE;
     }
 
     // 연산자가 제대로 입력되었는지 확인, ++ -- 두개씩 입력 시 에러
     if (argv[2][1] != '\0')
     {
         printf("Error, invalid operator.\n");
-        return -7;
+        return ERROR_INPUT_2_OPERATORS;
     }
 
     // 0으로 나눌 때 에러
     if ((operator == '/') && (second_operand == 0))
     {
         printf("Error, cannot be divided by 0.\n");
-        return -8;
+        return ERROR_NOT_DIVIDE_BY_0;
     }
 
     // +, -, X, / 연산자에 따라 해당 함수 호출
@@ -165,7 +182,7 @@ int main(int argc, char *argv[])
 
         default:
             printf("Error, use +, -, X, / operator\n");   // +, -, X, /가 아니면 오류 출력
-            return -9;
+            return ERROR_NOT_4_BASIC_OPERATORS;
     }
 
     // 파일에 결과를 저장
@@ -174,7 +191,7 @@ int main(int argc, char *argv[])
     if (fp == NULL)
     {
         printf ("Error open file");
-        return -10;
+        return ERROR_FOPEN_FAIL;
     }
 
     // 정수를 문자열로 변환하여 파일에 쓰기 (sprintf 사용)
@@ -191,5 +208,5 @@ int main(int argc, char *argv[])
     fwrite(result_string, sizeof(char), my_strlen(result_string), fp);
     fclose(fp);
 
-    return 0;
+    return ERROR_NONE;
 }
